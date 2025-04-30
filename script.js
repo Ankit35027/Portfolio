2984e00029b108a7a217dcb2f11e27aff0640ae3
Ankit <ankitgarg35027@gmail.com>
2025-04-30 10:16:45 +0530

    add some styling and optimised it for small devices

---
+++ script.js
@@ -0,0 +1,5 @@
+// const bio = document.querySelector(".Bio")
+//  // Clone the element (true means deep clone with child elements)
+//  const clone = bio.cloneNode(true);
+//  console.log(clone)
+//  document.body.append(clone)
